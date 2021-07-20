d337d7a9b46d33cbac5b1d1a4865c0fb26919468
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-07-20 07:54:29 -0500

    saving

---
+++ prodblog/src/pages/single/Single.js
@@ -0,0 +1,11 @@
+import './single.css'
+import Sidebar from '../../components/sidebar/Sidebar'
+export default function Single() {
+    return (
+        <div className="single">
+        
+            <Sidebar/>
+        </div>
+    )
+}
+// https://youtu.be/tlTdbc5byAs?t=2840
